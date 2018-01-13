2b1ed28a351e191d00aa2947f246ec28ec0c4c1d
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-13 18:08:50 +0100

    datacolletor, feeder, training data

--- datacollector.py
+++ datacollector.py
@@ -132,7 +132,7 @@ class Client():
 		self.trackname= 'unknown'
 		self.stage= 3 # 0=Warm-up, 1=Qualifying 2=Race, 3=unknown <Default=3>
 		self.debug= False
-		self.maxSteps= 100000  # 50steps/second
+		self.maxSteps= 50000  # 50steps/second
 		self.parse_the_command_line()
 		if H: self.host= H
 		if p: self.port= p
--- datafeeder.py
+++ datafeeder.py
@@ -1,21 +1,33 @@
 from __future__ import print_function, division
 import os
 import torch
-import pandas as pd
-from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torch.utils.data import Dataset
+import pickle as plk
 
-def load_state():
-	IMAGE = 0
-	STEER = 1
+class DrivingData(Dataset):
+	def __init__(self, file_name):
+		with open(file_name, 'rb') as file:
+			self.racedata = plk.load(file)
 
-	with open('file.txt', 'rb') as file:
-		racedata = plk.load(file)
+	def __getitem__(self, idx):
+		IMAGE = 0
+		STEER = IMAGE + 1
+		frame = self.racedata[idx]
+		input_data = frame[IMAGE]
+		label_data = float(frame[STEER])
 
-	for frame in racedata:
-		input = frame[IMAGE]
-		label = frame[STEER]
+		#if self.transform:
+	    #	sample = self.transform(sample)
 
+		return (input_data, label_data)
+
+	def __len__(self):
+		return len(self.racedata)
+
+
+#train_data = DrivingData('race1515861815.769681.txt')
+#train_loader = torch.utils.data.DataLoader(train_data, batch_size=2, shuffle=True, num_workers=0)
+
+#for i, (data, target) in enumerate(train_loader):
+# 	print(target[0])
