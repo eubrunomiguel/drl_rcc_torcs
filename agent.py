2da1655989e1f8274a55c255e688dd1389337028
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-13 13:07:03 +0100

    remove graph drawing

--- agent.py
+++ agent.py
@@ -31,9 +31,9 @@ class Agent(object):
         for i in range(3):
             img[:, :, i] = 255 - vision[:, i].reshape((64, 64))
 
-        plt.imshow(img, origin='lower')
-        plt.draw()
-        plt.pause(0.001)
+        # plt.imshow(img, origin='lower')
+        # plt.draw()
+        # plt.pause(0.001)
 
         # random action
         return np.tanh(np.random.randn(1))
