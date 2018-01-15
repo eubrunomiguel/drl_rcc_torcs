1637bf74b876022661b266a017045dbfe185abf7
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-15 15:17:19 +0100

    launcher

--- scripts/autostart.py
+++ scripts/autostart.py
@@ -4,11 +4,17 @@ import time
 class TorcsInstance:
 
 	def start(self):
-		print("Relaunch Torcs 2.1")
-		self.stop()
-		self.__wait(2)
-		self.__start()
-		self.__scriptboot()
+		# print("Relaunch Torcs 2.1")
+		# self.stop()
+		# self.__wait(2)
+		# self.__start()
+		# self.__scriptboot()
+		print("relaunc 2")
+		os.system(u'pkill torcs')
+		time.sleep(1.0)
+		os.system(u'torcs -nofuel -nodamage -nolaptime -vision &')
+		time.sleep(1.0)
+		os.system(u'sh autostart.sh')
 
 	def stop(self):
 		os.system('pkill torcs')
