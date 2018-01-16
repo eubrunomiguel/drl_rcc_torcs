c02bd10b31ef1fa4c45918ab770bb811ccf60242
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-16 16:44:12 +0100

    delay increase

--- scripts/autostart.py
+++ scripts/autostart.py
@@ -6,9 +6,9 @@ class TorcsInstance:
 	def start(self, random_track=False):
 		print("Relaunch Torcs 2.1")
 		os.system(u'pkill torcs')
-		time.sleep(1.0)
+		time.sleep(2.0)
 		os.system(u'torcs -nofuel -nodamage -nolaptime -vision &')
-		time.sleep(1.0)
+		time.sleep(2.0)
 		if random_track is True:
 			os.system(u'sh scripts/random_autostart.sh')
 		else:
