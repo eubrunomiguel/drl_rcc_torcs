3390e314ff355baf58e6f1ed0d0ac7bdcc2dc98d
YuqiShen <35395879+YuqiShen@users.noreply.github.com>
2018-01-18 17:13:14 +0100

    Actnn

--- Actnn.py
+++ Actnn.py
@@ -12,22 +12,36 @@ class Actnn(nn.Module):
         self.conv3 = nn.Conv2d(36,48,5,stride = 2)
         self.conv4 = nn.Conv2d(48,64,3)
         self.conv5 = nn.Conv2d(64,64,3)
-        self.fc1 = nn.Linear(64, 32)
+        self.fc1 = nn.Linear(64, 1)
+        '''
         self.fc2 = nn.Linear(32, 10)
         self.fc2 = nn.Linear(10, 1)
+        '''
         
     def forward(self, x):
-     
+ 
         out = F.relu(self.conv2(F.relu(self.conv1(x))));
-        out = F.relu(self.conv4(F.relu(self.conv3(x))));
+        out = F.relu(self.conv4(F.relu(self.conv3(out))));
         out = F.relu(self.conv5(out));
-        out = out.view(-1)
-        out = F.relu(self.fc1(out))
+        out = out.view(-1, self.num_flat_features(out))
+        out = F.tanh(self.fc1(out))
+        '''
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
+        '''
         
         return out
-
+    
+    def num_flat_features(self, x):
+        """
+        Computes the number of features if the spatial input x is transformed
+        to a 1D flat input.
+        """
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
     @property
     def is_cuda(self):
