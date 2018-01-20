c35f3a318580908ec5b6791ab1f6392fc86e4daa
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-20 14:42:46 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,18 +3,17 @@ from network import *
 import numpy as np
 import torch
 from torch.autograd import Variable
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
+train_data, val_data = getDrivingData(speed=30, track=0)
 
-train_data, val_data = getDrivingData("racingdata/#track=1#speed=30.txt")
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=5, shuffle=True, num_workers=0)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=50, shuffle=True, num_workers=0)
 
 network = DrivingNN()
 objectivefunction = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(network.parameters())
 
-# plt.axis([0, 10, 0, 1])
-# plt.ion()
+num_epochs = 100
 
 def getAccuracy(label, output, range_percentage):
 	num_examples = label.shape[0]
@@ -31,8 +30,8 @@ def getAccuracy(label, output, range_percentage):
 accuracy_history = []
 loss_history = []
 
-for epoch in range(1):
-	loss_history = []
+for epoch in range(num_epochs):
+	loss = []
 	accuracy = []
 	for i, (data, target) in enumerate(train_loader):
 		optimizer.zero_grad()
@@ -42,9 +41,26 @@ for epoch in range(1):
 		cost = objectivefunction(output_value, targets)
 		cost.backward()
 		optimizer.step()
-		loss_history.append(cost.data[0])
+		loss.append(cost.data[0])
 		accuracy += getAccuracy(targets, output_value, 10)
-	print(np.mean(accuracy))
+	print("EPOCH %d/%d" % (epoch, num_epochs))
+	loss_history.append(np.mean(loss))
+	accuracy_history.append(np.mean(accuracy))
 
 
+# Plot the loss function and train / validation accuracies
+plt.subplots(nrows=2, ncols=1)
 
+plt.subplot(2, 1, 1)
+plt.plot(loss_history)
+plt.title('Loss history')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+
+plt.subplot(2, 1, 2)
+plt.plot(accuracy_history, label='train')
+plt.title('Classification accuracy history')
+plt.xlabel('Epoch')
+plt.ylabel('Classification accuracy')
+plt.tight_layout()
+plt.show()
