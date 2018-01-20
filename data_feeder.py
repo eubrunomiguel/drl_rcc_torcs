0762df352ca68055f768c8b22dafec34d7cf2a7b
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-20 16:20:35 +0100

    Update data_feeder.py

--- data_feeder.py
+++ data_feeder.py
@@ -113,7 +113,8 @@ def getDrivingData(speed=0, track=0, num_training_percentage=80, num_validation_
 	X -= np.mean(X, axis=0)
 
 	# Transpose so that channels come first
-	X = X.transpose(0, 3, 1, 2)
+	X_train = X_train.transpose(0, 3, 1, 2)
+	X_val = X_val.transpose(0, 3, 1, 2)
 
 	print("Number of training examples %d" % (X_train.shape[0]))
 
