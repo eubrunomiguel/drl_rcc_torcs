681ad0387bb691a2777676f1532945a010beada7
eubrunomiguel <bruno-miguel1994@hotmail.com>
2018-01-22 14:50:43 +0100

    update

--- tester.py
+++ tester.py
@@ -5,8 +5,6 @@ import torch
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-#hello2
-
 train_data, val_data = getDrivingData(speed=30, track=0)
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=50, shuffle=True, num_workers=0)
